5d24ea4d0f51ea2ac60f1cc0d0d19cb825889740
= <thiagoespindula00@gmail.com>
2024-12-27 16:00:39 -0300

    Consumido mensagens do RabbitMQ

---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,14 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PagamentoListener {
+    @RabbitListener(queues = "pagamento.concluido")
+    public void recebeMensagem(Message message) {
+        System.out.println("Mensagem recebida -> " + message.getBody().toString());
+    }
+}
