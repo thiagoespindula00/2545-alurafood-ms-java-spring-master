b9682d8db41b07d094da999cc7e5fddf5b5414f0
= <thiagoespindula00@gmail.com>
2024-12-28 12:24:28 -0300

    Consumindo JSON do pagamento

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,5 +1,6 @@
 package br.com.alurafood.pedidos.amqp;
 
+import br.com.alurafood.pedidos.dto.PagamentoDTO;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class PagamentoListener {
     @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message message) {
-        System.out.println("Mensagem recebida -> " + message.getBody().toString());
+    public void recebeMensagem(PagamentoDTO pagamentoDTO) {
+        String mensagem = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s
+                """.formatted(pagamentoDTO.getId(), pagamentoDTO.getPedidoId(), pagamentoDTO.getValor(), pagamentoDTO.getStatus());
+
+        System.out.println(mensagem);
     }
 }
