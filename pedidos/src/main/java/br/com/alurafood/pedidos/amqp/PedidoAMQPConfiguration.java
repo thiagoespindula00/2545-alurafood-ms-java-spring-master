4a9af8a867636bb96e50dcd7cfc9ac7ec395eb6d
= <thiagoespindula00@gmail.com>
2024-12-30 11:14:12 -0300

    Alterado para criar a fila e atrelar a exchange no PedidoAMQPConfiguration

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
@@ -1,8 +1,12 @@
 package br.com.alurafood.pedidos.amqp;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,5 +21,36 @@ public class PedidoAMQPConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue filaDetalhesPedido() {
+        return QueueBuilder
+                .nonDurable("pagamentos.detalhes-pedido")
+                .build();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return ExchangeBuilder
+                .fanoutExchange("pagamentos.exchange")
+                .build();
+    }
+
+    @Bean
+    public Binding binding(FanoutExchange fanoutExchange) {
+        return BindingBuilder
+                .bind(filaDetalhesPedido())
+                .to(fanoutExchange);
+    }
+
+    @Bean
+    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
     }
 }
